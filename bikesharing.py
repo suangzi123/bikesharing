9c0f6472aa7dba2dcf3bbd6bbd33554f0d5bffa4
suangzi123 <suangzi123@sina.com>
2018-05-02 22:41:36 +0800

    5/2 commit

--- bikesharing.py
+++ bikesharing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-dataframe=pd.read_csv("D:\\bikesharing\\data\\train.csv")
+dataframe=pd.read_csv("train.csv")
 sns.set()
 
 
