78a143b54ca387b24e7ea215b61d7059d59b1859
suangzi123 <suangzi123@sina.com>
2018-05-08 19:35:43 +0800

    5/8

--- bikesharing.py
+++ bikesharing.py
@@ -7,13 +7,14 @@ sns.set()
 
 
 dataframe["hour"]=pd.DataFrame(dataframe.datetime.apply(lambda x:x.split()[1].split(":")[0]))
+dataframe['hour'] = dataframe['hour'].astype('int64')
 dataframe["month"]=pd.DataFrame(dataframe.datetime.apply(lambda x:x.split()[0].split("-")[1]))
+dataframe['month'] = dataframe['month'].astype('int64')
 dataframe["season"]=dataframe.season.map({1:"spring",2:"summer",3:"fall",4:"winter"})
-dataframe["workingday"]=dataframe.workingday.map({0:"no",1:"yes"})
+#dataframe["workingday"]=dataframe.workingday.map({0:"no",1:"yes"})
 dataframe["weather"]=dataframe.weather.map({1:"Clear",2:"Mist + Cloudy",3:"Light Snow",4:"Heavy Rain"})
 
 #Count distribution
-
 quartiles = pd.cut(dataframe["count"],10)
 def get_stats(group):
     return {'counts': group.sum()}
@@ -21,9 +22,6 @@ def get_stats(group):
 grouped = dataframe["count"].groupby(quartiles)
 count_distribution_amount = grouped.apply(get_stats).unstack()
 df=pd.DataFrame(count_distribution_amount)
-
-
-
 df.plot(kind='bar',figsize=(9, 7),rot=45)
 plt.title("count distribution")
 plt.show()
@@ -31,29 +29,35 @@ plt.show()
 
 
 #Removal of outliers 
-
 upper_bound=dataframe['count'].mean()+3*dataframe['count'].std()
 lower_bound=dataframe['count'].mean()-3*dataframe['count'].std()
 dataframe=dataframe[dataframe["count"]<upper_bound]
 dataframe=dataframe[dataframe["count"]>lower_bound]
 
+
+#Correlation matrix
+corr_data=dataframe[['hour','month','temp','atemp','humidity','windspeed','casual','registered','count']]
+corr = corr_data.corr()
+corr=corr.round(2)
+f, ax = plt.subplots(figsize=(9, 6))
+sns.heatmap(corr, xticklabels=corr.columns,yticklabels=corr.columns,annot=True,cmap='coolwarm')
+label_y = ax.get_yticklabels()
+plt.setp(label_y, rotation=360, horizontalalignment='right')
+label_x = ax.get_xticklabels()
+plt.setp(label_x, rotation=45, horizontalalignment='right')
+plt.title("Correlation matrix")
+plt.show()
+
+
+
+
+
+
 #weather statistics
-
-Clear_casual=dataframe[dataframe["weather"]=="Clear"]["casual"].sum()
-Mist_casual=dataframe[dataframe["weather"]=="Mist + Cloudy"]["casual"].sum()
-Snow_casual=dataframe[dataframe["weather"]=="Light Snow"]["casual"].sum()
-Rain_casual=dataframe[dataframe["weather"]=="Heavy Rain"]["casual"].sum()
-
-Clear_registered=dataframe[dataframe["weather"]=="Clear"]["registered"].sum()
-Mist_registered=dataframe[dataframe["weather"]=="Mist + Cloudy"]["registered"].sum()
-Snow_registered=dataframe[dataframe["weather"]=="Light Snow"]["registered"].sum()
-Rain_registered=dataframe[dataframe["weather"]=="Heavy Rain"]["registered"].sum()
-
-index=["Clear","Mist","Snow","Rain"]
-values1=[Clear_registered,Mist_registered,Snow_registered,Rain_registered]
-values2=[Clear_casual,Mist_casual,Snow_casual,Rain_casual]
-plt.bar(index,values1,color='blue')
-plt.bar(index,values2,color='red',bottom=values1)
+casual=dataframe.groupby('weather')['casual'].sum()
+registered=dataframe.groupby('weather')['registered'].sum()
+plt.bar(registered.index,registered.values,color='blue')
+plt.bar(casual.index,casual.values,color='red',bottom=registered.values)
 plt.title("Weather statistics bar chart ")
 plt.legend(["number of registered user ","number of non-registered user"])
 plt.show()
@@ -61,11 +65,11 @@ plt.show()
 
 
 # hours statistics with weather
-
 clear_data=dataframe[dataframe["weather"]=="Clear"]
 mist_data=dataframe[dataframe["weather"]=="Mist + Cloudy"]
 snow_data=dataframe[dataframe["weather"]=="Light Snow"]
 rain_data=dataframe[dataframe["weather"]=="Heavy Rain"]
+
 
 y1=clear_data.groupby('hour')['count'].mean()
 y2=mist_data.groupby('hour')['count'].mean()
@@ -123,14 +127,5 @@ plt.show()
 
 
 
-#Correlation matrix
-corr = dataframe.corr()
-corr=corr.round(2)
-f, ax = plt.subplots(figsize=(9, 6))
-sns.heatmap(corr, xticklabels=corr.columns,yticklabels=corr.columns,annot=True,cmap='coolwarm')
-label_y = ax.get_yticklabels()
-plt.setp(label_y, rotation=360, horizontalalignment='right')
-label_x = ax.get_xticklabels()
-plt.setp(label_x, rotation=45, horizontalalignment='right')
-plt.title("Correlation matrix")
-plt.show()
+
+
