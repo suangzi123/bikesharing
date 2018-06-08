6f2bb3bb5da2e1953ae095c27ec4a5a00a9cbeae
suangzi123 <suangzi123@sina.com>
2018-06-09 03:54:05 +0800

    clean code

--- bikesharing/MeasurePreprocess.py
+++ bikesharing/MeasurePreprocess.py
@@ -3,8 +3,12 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder,StandardScaler
 from sklearn.model_selection import train_test_split
 def GetData():
-    dataset=pd.read_csv("train.csv")
-    dataset=dataset[['season','holiday','workingday','weather','temp','humidity','windspeed','count']]
+    dataset=pd.read_csv("D:\\bikesharing\\data\\train.csv")
+    dataset["hour"]=pd.DataFrame(dataset.datetime.apply(lambda x:x.split()[1].split(":")[0]))
+    dataset['hour'] = dataset['hour'].astype('int64')
+    dataset["month"]=pd.DataFrame(dataset.datetime.apply(lambda x:x.split()[0].split("-")[1]))
+    dataset['month'] = dataset['month'].astype('int64')
+    dataset=dataset[['season','holiday','workingday','weather','hour','month','temp','humidity','windspeed','count']]
 
     upper_bound=dataset['count'].mean()+3*dataset['count'].std()
     lower_bound=dataset['count'].mean()-3*dataset['count'].std()
@@ -15,38 +19,43 @@ def GetData():
 
     
     labelencoder = LabelEncoder()
-    onehotencoder = OneHotEncoder(categorical_features = range(4))
+    onehotencoder = OneHotEncoder(categorical_features = range(6))
     X = onehotencoder.fit_transform(X).toarray()
-    scaler = StandardScaler()
-    scaler.fit(X)
-    X=scaler.transform(X)
-    X=X[:,[2,3,9,12,13,14]]
+    #X=np.delete(X,[3,10,11,23,26,37,38,39,41],axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     return X_train, X_test, y_train, y_test
 
-def GetWindspeedData():
+def GetHourData():
     dataset=pd.read_csv("D:\\bikesharing\\data\\train.csv")
-    dataset=dataset[['windspeed','count']]
+    dataset["hour"]=pd.DataFrame(dataset.datetime.apply(lambda x:x.split()[1].split(":")[0]))
+    dataset['hour'] = dataset['hour'].astype('int64')
+    dataset=dataset[['hour','count']]
     X = dataset.iloc[:, :-1].values
     y = dataset.iloc[:, -1].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     return X_train, X_test, y_train, y_test
 
-def WindspeedPolynomialRegression():
+def HourPolynomialRegression():
     dataset=pd.read_csv("D:\\bikesharing\\data\\train.csv")
-    dataset=dataset[['count','windspeed']]
-    dataset['square of windspeed']=dataset.windspeed.values**2
-    dataset['cube of windspeed']=dataset.windspeed.values**3
-    X = dataset.iloc[:, 1:].values
-    y = dataset.iloc[:, 0].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=0)
+    dataset["hour"]=pd.DataFrame(dataset.datetime.apply(lambda x:x.split()[1].split(":")[0]))
+    dataset['hour'] = dataset['hour'].astype('int64')
+    dataset['square of hour']=dataset.hour.values**2
+    dataset['cube of hour']=dataset.hour.values**3
+    dataset=dataset[['hour','square of hour','cube of hour','count']]
+    X = dataset.iloc[:, :-1].values
+    y = dataset.iloc[:, -1].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     return X_train, X_test, y_train, y_test
 
    
 
+
 def ErrorRate(test,pred):
-    avg=[None]*len(test)
+    avg=[10]*len(test)
     for i in range(len(test)):
-        avg[i]=abs(float(test[i]) - float(pred[i]))/float(test[i])
+        try:
+            avg[i]=abs(float(test[i]) - float(pred[i]))/float(test[i])
+        except ZeroDivisionError,e:
+            print e.message
     avg=np.array(avg)
     return avg.mean()
